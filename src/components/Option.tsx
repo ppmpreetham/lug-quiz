e7633b49960945b322f7efe797b64afdac69802e
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-02-25 01:25:29 +0530

    fix: option full width

--- src/components/Option.tsx
+++ src/components/Option.tsx
@@ -1,10 +1,10 @@
 const Option = ({option, no}:{option: string, no:number}) => {
   return (
-    <div className="flex flex-row text-white text-4xl items-center gap-4 w-fit">
-        <div className="border-white border h-16 flex items-center hover:bg-green-600 hover:text-black p-2">
-            [{String.fromCharCode(no+64)}]
+    <div className="flex flex-row text-white text-4xl items-center gap-4 w-full px-6">
+        <div className="border-white border h-16 flex items-center hover:bg-green-600 hover:text-black py-2 px-4">
+            {String.fromCharCode(no+64)}
         </div>
-        <div className='border-white border h-16 w-96 text-center flex items-center justify-center hover:bg-green-600 hover:text-black hover:border-none flex-row p-2'>
+        <div className='border-white border h-16 w-full text-center flex items-center justify-center hover:bg-green-600 hover:text-black hover:border-none flex-row p-2 cursor-pointer'>
             {option}
         </div>
     </div>
