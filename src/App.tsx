fd7620cd8ec1de13fa448ee709327fc434df82b1
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-02-25 01:26:29 +0530

    add: basic questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
 import Option from "./components/Option"
+import {Question} from "./types"
+import QuestionComp from "./components/Question"
+// Multiple questions
 
-// Multiple questions, core
+const questions: Question[] = [
+  {
+    question: "What is the capital of France?",
+    options: ["New York", "London", "Paris", "Dublin"],
+    answer: ["Paris", "New York"]
+  },
+  {
+    question: "Who is CEO of Tesla?",
+    options: ["Elon Musk", "Jeff Bezos", "Bill Gates", "Tony Stark"],
+    answer: ["Elon Musk"]
+  },
+  {
+    question: "The iPhone was created by which company?",
+    options: ["Apple", "Intel", "Amazon", "Microsoft"],
+    answer: ["Apple"]
+  },
+  {
+    question: "How many Harry Potter books are there?",
+    options: ["1", "4", "6", "7"],
+    answer: ["7"]
+  }
+]
 
+const getLatestQuestion = (questions: Question[]): Question => {
+  return questions[questions.length - 1]
+} 
 
 function App() {
   return (
     <>
       <div className="h-screen w-screen bg-black">
-        <Option no={1} option="HELLo"/>
+        <QuestionComp qn={getLatestQuestion(questions).question} options={getLatestQuestion(questions).options}/>
       </div>
     </>
   )
