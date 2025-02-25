1794dff267ce1f3fe07d5a08f02910718cd4ed1f
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-02-25 14:31:18 +0530

    add: navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,16 +1,56 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="flex flex-row justify-between items-center py-8 px-16 bg-black w-screen">
-      <div className="text-white text-2xl">LugQuiz</div>
-      <div className="flex flex-row gap-8 uppercase">
-        <Link to="/leaderboard" className="text-white text-lg">Leaderboard</Link>
-        <Link to="/questions" className="text-white text-lg">Questions</Link>
-        <Link to="/profile" className="text-white text-lg">Profile</Link>
+    <nav className="bg-black p-4 w-full overflow-x-hidden">
+      <div className="flex justify-between items-center max-w-screen-xl mx-auto">
+        <Link to="/" className="text-white text-4xl md:text-2xl">
+          LUGQUIZ
+        </Link>
+        <div className="md:hidden">
+          <button
+            onClick={toggleMenu}
+            className="text-white focus:outline-none"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`flex-col md:flex md:flex-row gap-8 uppercase ${isOpen ? "flex" : "hidden"}`}
+        >
+          <Link to="/leaderboard" className="text-white text-lg">
+            Leaderboard
+          </Link>
+          <Link to="/questions" className="text-white text-lg">
+            Questions
+          </Link>
+          <Link to="/profile" className="text-white text-lg">
+            Profile
+          </Link>
+        </div>
       </div>
-    </div>
+    </nav>
   );
-}
+};
 
 export default Nav;
