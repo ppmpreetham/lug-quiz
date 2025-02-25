e563f5692590a1d37d1491dab9f139bb2d4ce89d
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-02-25 12:34:10 +0530

    fix: select over hover priority

--- src/components/Option.tsx
+++ src/components/Option.tsx
@@ -1,10 +1,10 @@
-const Option = ({option, no}:{option: string, no:number}) => {
+const Option = ({option, no, isSelected}:{option: string, no:number, isSelected: boolean}) => {
   return (
     <div className="flex flex-row text-white text-4xl items-center gap-4 w-full px-6 group">
-      <div className="border-white border h-16 flex items-center py-2 px-4 group-hover:bg-white group-hover:text-black">
+      <div className={`border-white border h-16 flex items-center py-2 px-4 ${isSelected ? 'bg-green-600 text-black' : 'group-hover:bg-white group-hover:text-black'}`}>
         {String.fromCharCode(no + 64)}
       </div>
-      <div className='border-white border h-16 w-full text-center flex items-center justify-center hover:bg-green-600 hover:text-black hover:border-none flex-row p-2 cursor-pointer'>
+      <div className={`border-white border h-16 w-full text-center flex items-center justify-center flex-row p-2 cursor-pointer ${isSelected ? 'bg-green-600 text-black border-none' : 'group-hover:bg-white group-hover:text-black group-hover:border-none cursor-pointer'}`}>
         {option}
       </div>
     </div>
