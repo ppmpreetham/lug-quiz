1c5a8dd00f612e7e4001da74193f11bb430c843e
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-02-25 14:31:38 +0530

    update: question

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,27 +1,38 @@
-import Option from "./Option"
-import { useState } from "react"
+import Option from "./Option";
+import { useState } from "react";
 
-const QuestionComp = ({qn, options, date}:{qn: string, options: string[], date:string}) => {
-
-  const [selected, setSelected] = useState<number | null>(null)  
+const QuestionComp = ({
+  qn,
+  options,
+  date,
+}: {
+  qn: string;
+  options: string[];
+  date: string;
+}) => {
+  const [selected, setSelected] = useState<number | null>(null);
 
   const handleOptionClick = (index: number) => {
-    setSelected(index)
-  }
+    setSelected(index);
+  };
 
   return (
-    <div className="flex flex-col gap-10 p-10 text-white">
-        <div className="text-4xl text-center">{date}</div>
-        <div className="text-4xl text-center">{qn}</div>
-        <div className="flex flex-col gap-4">
-            {options.map((option, index) => (
-            <div key={index} onClick={() => handleOptionClick(index)}>
-                <Option option={option} no={index+1} isSelected={selected === index}/>
-            </div>
-            ))}
-        </div>
+    <div className="flex flex-col gap-10 p-10 text-white w-full">
+      <div className="text-4xl text-center">{date}</div>
+      <div className="text-4xl text-center">{qn}</div>
+      <div className="flex flex-col gap-4">
+        {options.map((option, index) => (
+          <div onClick={() => handleOptionClick(index)}>
+            <Option
+              option={option}
+              no={index + 1}
+              isSelected={selected === index}
+            />
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default QuestionComp;
