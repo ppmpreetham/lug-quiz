1dab09537f05b2ab05eedec951e3da506ab0b762
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-02-25 12:34:31 +0530

    add: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,53 @@
-import {Question} from "./types"
-import QuestionComp from "./components/Question"
-import Nav from "./components/Nav"
-import FetchProfilePicture from "./utils/FetchProfilePicture"
-// Multiple questions
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Question } from './types';
+import QuestionComp from './components/Question';
+import Nav from './components/Nav';
+import QuestionCard from './components/QuestionCard';
 
+// Multiple questions
 const questions: Question[] = [
   {
-    question: "What is the capital of France?",
-    options: ["New York", "London", "Paris", "Dublin"],
-    answer: ["Paris", "New York"]
+    question: 'What is the capital of France?',
+    options: ['New York', 'London', 'Paris', 'Dublin'],
+    answer: ['Paris', 'New York'],
+    date: '2025-01-01'
   },
   {
-    question: "Who is CEO of Tesla?",
-    options: ["Elon Musk", "Jeff Bezos", "Bill Gates", "Tony Stark"],
-    answer: ["Elon Musk"]
+    question: 'Who is CEO of Tesla?',
+    options: ['Elon Musk', 'Jeff Bezos', 'Bill Gates', 'Tony Stark'],
+    answer: ['Elon Musk'],
+    date: '2025-01-01'
   },
   {
-    question: "The iPhone was created by which company?",
-    options: ["Apple", "Intel", "Amazon", "Microsoft"],
-    answer: ["Apple"]
+    question: 'The iPhone was created by which company?',
+    options: ['Apple', 'Intel', 'Amazon', 'Microsoft'],
+    answer: ['Apple'],
+    date: '2025-01-01'
   },
   {
-    question: "How many Harry Potter books are there?",
-    options: ["1", "4", "6", "7"],
-    answer: ["7"]
-  }
-]
+    question: 'How many Harry Potter books are there?',
+    options: ['1', '4', '6', '7'],
+    answer: ['7'],
+    date: '2025-01-01'
+  },
+];
 
 const getLatestQuestion = (questions: Question[]): Question => {
-  return questions[questions.length - 1]
-} 
+  return questions[questions.length - 1];
+};
 
 function App() {
   return (
-    <>
-      <FetchProfilePicture username={"ppmpreetham"}/>
+    <Router>
       <div className="h-screen w-screen bg-black">
         <Nav />
-        <QuestionComp qn={getLatestQuestion(questions).question} options={getLatestQuestion(questions).options}/>
+        <Routes>
+          <Route path="/" element={<QuestionComp qn={getLatestQuestion(questions).question} options={getLatestQuestion(questions).options} date={getLatestQuestion(questions).date}/>}/>
+          <Route path='/questions' element={<QuestionCard question={questions[0]}/>}/>
+        </Routes>
       </div>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Nav = () => {
   return (
     <div className="flex flex-row justify-between items-center py-8 px-16 bg-black w-screen">
-        <div className="text-white text-2xl">LugQuiz</div>
-        <div className="flex flex-row gap-8 uppercase">
-            <div className="text-white text-lg">Leaderboard</div>
-            <div className="text-white text-lg">Questions</div>
-            <div className="text-white text-lg">Profile</div>
-        </div>
+      <div className="text-white text-2xl">LugQuiz</div>
+      <div className="flex flex-row gap-8 uppercase">
+        <Link to="/leaderboard" className="text-white text-lg">Leaderboard</Link>
+        <Link to="/questions" className="text-white text-lg">Questions</Link>
+        <Link to="/profile" className="text-white text-lg">Profile</Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
