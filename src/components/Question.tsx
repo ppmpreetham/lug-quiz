55634b64ba25341d5f7150329a872e8e25596774
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-02-25 12:34:50 +0530

    handle click on question

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,13 +1,22 @@
 import Option from "./Option"
+import { useState } from "react"
 
-const QuestionComp = ({qn, options}:{qn: string, options: string[]}) => {
+const QuestionComp = ({qn, options, date}:{qn: string, options: string[], date:string}) => {
+
+  const [selected, setSelected] = useState<number | null>(null)  
+
+  const handleOptionClick = (index: number) => {
+    setSelected(index)
+  }
+
   return (
-    <div className="flex flex-col gap-10 p-10">
-        <div className="text-white text-4xl text-center">{qn}</div>
+    <div className="flex flex-col gap-10 p-10 text-white">
+        <div className="text-4xl text-center">{date}</div>
+        <div className="text-4xl text-center">{qn}</div>
         <div className="flex flex-col gap-4">
             {options.map((option, index) => (
-            <div key={index} >
-                <Option option={option} no={index+1}/>
+            <div key={index} onClick={() => handleOptionClick(index)}>
+                <Option option={option} no={index+1} isSelected={selected === index}/>
             </div>
             ))}
         </div>
@@ -15,4 +24,4 @@ const QuestionComp = ({qn, options}:{qn: string, options: string[]}) => {
   )
 }
 
-export default QuestionComp
+export default QuestionComp;
