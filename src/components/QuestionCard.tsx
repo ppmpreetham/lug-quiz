f5d7199f0af7b888e857b56e2be1415d58f66ebc
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-02-25 14:32:05 +0530

    update: questioncard

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -2,11 +2,11 @@ import { Question } from "../types";
 
 const QuestionCard = ({ question }: { question: Question }) => {
   return (
-    <div className="text-white border border-white p-8 m-8 w-[30vw] flex flex-col gap-4">
-      <div className="flex flex-row justify-between uppercase">
-        <p className="text-2xl">{question.date}</p>
+    <div className="text-white border border-white p-4 sm:p-6 lg:p-8 w-full lg:w-[30vw] flex flex-col gap-4">
+      <div className="flex flex-row justify-between uppercase w-full">
+        <p className="text-lg sm:text-xl lg:text-2xl">{question.date}</p>
         <p
-          className={`text-2xl ${(() => {
+          className={`text-lg sm:text-xl lg:text-2xl ${(() => {
             switch (question.difficulty?.toLowerCase()) {
               case "easy":
                 return "text-green-500";
@@ -22,20 +22,18 @@ const QuestionCard = ({ question }: { question: Question }) => {
           {question.difficulty}
         </p>
       </div>
-      <h2 className="text-4xl">{question.question}</h2>
+      <h2 className="text-2xl sm:text-3xl lg:text-4xl">{question.question}</h2>
       <ul>
         {question.options.map((option, index) => (
-          <div>
-            <li
-              key={index}
-              className={`border border-white p-2 my-4 ${question.answer !== undefined && question.answer + 1 === index ? "bg-green-600 text-black" : ""}`}
-            >
-              {option}
-            </li>
-          </div>
+          <li
+            key={index}
+            className={`border border-white p-2 my-2 sm:my-3 lg:my-4 ${question.answer != null && question.answer - 1 === index ? "bg-green-600 text-black" : ""}`}
+          >
+            {option}
+          </li>
         ))}
       </ul>
-      <p className="text-xl">{question.explanation}</p>
+      <p className="text-lg sm:text-xl">{question.explanation}</p>
     </div>
   );
 };
